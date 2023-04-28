146349ae7bb7ee969d5b3633c5f9ef28fdeabde9
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2023-04-28 11:44:09 +0200

    Get the story based on its id from the api / .split the string to see the text id and returns as a array

--- pages/item.js
+++ pages/item.js
@@ -1,5 +1,11 @@
 import view from "../utils/view.js";
 
 export default function Item() {
+  getStory();
   view.innerHTML = `<div>item</div>`;
+}
+
+function getStory() {
+  const storyId = window.location.hash.split("?id=")[1];
+  console.log(storyId);
 }
