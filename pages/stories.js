94aa60da4e55c7fd8008b347928dc6d6634a1249
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2023-04-28 09:45:13 +0200

    Connect api data with route paths

--- pages/stories.js
+++ pages/stories.js
@@ -2,15 +2,20 @@ import view from "../utils/view.js";
 
 export default async function Stories(path) {
   const stories = await getStories(path);
-  view.innerHTML = `<div>${path}</div>`;
   console.log(stories);
+
+  view.innerHTML = `<div>${path}</div>`;
 }
 
 async function getStories(path) {
   const isHomeRoute = path === "/";
+  const isNewRoute = path === "/new";
   if (isHomeRoute) {
     path = "/news";
+  } else if (isNewRoute) {
+    path = "/newest";
   }
+
   const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
   const stories = await response.json();
   return stories;
