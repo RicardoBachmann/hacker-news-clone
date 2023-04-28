97dad11dff124fc66e74c03e2fd6c044297bcd20
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2023-04-28 12:08:32 +0200

    Fetch the individually story per id / reuse Story.js component

--- pages/item.js
+++ pages/item.js
@@ -1,11 +1,18 @@
+import Story from "../components/Story.js";
 import view from "../utils/view.js";
 
-export default function Item() {
-  getStory();
-  view.innerHTML = `<div>item</div>`;
+export default async function Item() {
+  const story = await getStory();
+
+  view.innerHTML = `<div>${Story(story)}</div>`;
 }
 
-function getStory() {
+async function getStory() {
   const storyId = window.location.hash.split("?id=")[1];
-  console.log(storyId);
+
+  const response = await fetch(
+    `https://node-hnapi.herokuapp.com/item/${storyId}`
+  );
+  const story = await response.json();
+  return story;
 }
