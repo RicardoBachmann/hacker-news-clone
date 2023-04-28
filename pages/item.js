a8aecf16005af44cbe2a3015fe58edf3ac4bfe01
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2023-04-28 12:30:36 +0200

    Formatting comments data with .map()

--- pages/item.js
+++ pages/item.js
@@ -3,13 +3,23 @@ import view from "../utils/view.js";
 
 export default async function Item() {
   const story = await getStory();
+  const hasComments = story.comments.length > 0;
 
-  view.innerHTML = `<div>${Story(story)}</div>`;
+  view.innerHTML = `
+  <div>
+    ${Story(story)}
+  </div>
+  <hr/>
+  ${
+    hasComments
+      ? story.comments.map((comment) => JSON.stringify(comment)).join("")
+      : "No comments"
+  }
+  `;
 }
 
 async function getStory() {
   const storyId = window.location.hash.split("?id=")[1];
-
   const response = await fetch(
     `https://node-hnapi.herokuapp.com/item/${storyId}`
   );
